e004c18e427984b67a3a4381b4a99e7434910d2d
semantic-release <semantic-release>
2024-10-12 13:14:12 +0000

    1.1.11
    Automatically generated by python-semantic-release

--- metabolights_utils/__init__.py
+++ metabolights_utils/__init__.py
@@ -158,7 +158,7 @@ from metabolights_utils.utils.hash_utils import (
 )
 from metabolights_utils.utils.search_utils import MetabolightsSearchUtils
 
-__VERSION__ = "1.1.10"
+__VERSION__ = "1.1.11"
 
 __all__ = [
     "Assay",
