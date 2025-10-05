72903c08c3bfa215b93ca6fbf7d8b90d02983ca7
oyurekten <ozgury@ebi.ac.uk>
2025-10-05 17:05:14 +0100

    fix: get study schema does not get version parameter.

--- metabolights_utils/models/metabolights/__init__.py
+++ metabolights_utils/models/metabolights/__init__.py
@@ -4,13 +4,19 @@ from typing import Dict, Union
 
 from metabolights_utils.models.metabolights import metabolights_study, model
 
+default_version = "1.1"
 
-def get_study_model_schema(schema_path: Union[str, None] = None) -> Dict:
+
+def get_study_model_schema(
+    schema_path: Union[str, None] = None, version: None | str = None
+) -> Dict:
     if not schema_path:
-        schema_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "study_model_schema_v1.1.json",
-        )
+        if version:
+            filename = f"study_model_schema_v{version}.json"
+        else:
+            filename = f"study_model_schema_v{default_version}.json"
+        schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+
     with open(schema_path, "r", encoding="utf-8") as sf:
         study_model_jsonschema = json.load(sf)
     return study_model_jsonschema
